49110d841e0060363dcbb9d7abd53c8bfca8a09b
Wulkain <zoombievi@gmail.com>
2022-09-19 18:33:56 +0300

    HW7,8,9

---
+++ HW8/Program.cs
@@ -0,0 +1,158 @@
+﻿/* Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2 */
+
+System.Console.WriteLine("Enter number of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Enter number of cols: ");
+int cols = Convert.ToInt32(Console.ReadLine());
+
+
+int[,] GetArray (int rows, int cols)
+{
+
+    int[,] array = new int [rows,cols];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            array[i,j] = new Random().Next(1 , 10);
+        }
+    }
+    return array;
+}
+
+int[,] SortOfArray (int[,] array)
+{
+    int tmp = array[0,0];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = j; k < array.GetLength(1); k++)
+            {
+                if(array[i,j] < array[i,k]) 
+                {
+                    tmp = array[i,k];
+                    array[i,k] = array[i,j];
+                    array[i,j] = tmp;
+                }
+            }
+        }
+    }
+    return array;
+}
+
+void ArrayOut (int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i,j]);
+        }
+        System.Console.WriteLine();
+    }
+}
+int[,] array1 = GetArray(rows, cols);
+
+ArrayOut(array1);
+
+System.Console.WriteLine();
+
+System.Console.WriteLine();
+
+ArrayOut(SortOfArray(array1));
+
+/* Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет
+находить строку с наименьшей суммой элементов. Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки
+с наименьшей суммой элементов: 1 строка */
+
+int minOfArray = EnterIntNumber("Введите минимальное число массива по умолчанию");
+int maxOfArray = EnterIntNumber("Введите максимальное число массива по умолчанию");
+
+int EnterIntNumber(string text) 
+{
+    Console.Write($"{text}...\n");
+
+    while (true)
+    {
+        try
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            return number;
+        }
+
+        catch (FormatException)
+        {
+            Console.WriteLine($"Некорректный ввод. Введите целое число!\n");
+        }
+    }
+}
+int[,] GetArray2(int rows = 5, int columns = 3, int numMin = 1, int numMax = 9) 
+{
+    int[,] newArray = new int[rows, columns];
+    var rand = new Random();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            newArray[i, j] = rand.Next(numMin, numMax + 1);
+        }
+    }
+
+    return newArray;
+}
+
+int[,] array2 = GetArray2(rows, cols, minOfArray, maxOfArray);
+
+System.Console.WriteLine($"\nМассив №2:\n");
+ArrayOut(array2);
+Console.WriteLine();
+
+FindMinSumOfArray(array2);
+
+void FindMinSumOfArray(int[,] arr) 
+{
+    int[] arrSumOfRow = new int[arr.GetLength(0)];
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arrSumOfRow[i] += arr[i, j];
+        }
+
+        Console.WriteLine($"Сумма элементов строки {i + 1} = {arrSumOfRow[i]}");
+    }
+
+    int minSum = arrSumOfRow[0];
+    int indexOfMinRow = 0;
+
+    for (int i = 1; i < arrSumOfRow.Length; i++)
+    {
+        if (arrSumOfRow[i] < minSum)
+        {
+            minSum = arrSumOfRow[i];
+            indexOfMinRow = i;
+        }
+    }
+
+    Console.WriteLine($"\nСтрока массива с наименьшей суммой элементов ({minSum}) - № {indexOfMinRow + 1}\n");
+}
+
---
+++ Hw9/Program.cs
@@ -0,0 +1,75 @@
+﻿
+
+/* string numberSeries(int M, int n)
+{
+    if (M <= n) return $"{M} " + numberSeries(M + 1, n);
+    else return String.Empty;
+}
+
+int SumSeries(int M, int n)
+{
+    if (M < n) return M + SumSeries(M + 1, n);
+    else return n;
+}
+
+ulong AkkermanFunction(ulong M = 2, ulong n = 3)
+{
+    if (M == 0 && (n != 0)) return n + 1;
+    if ((n == 0) && (M != 0)) return AkkermanFunction(M - 1, 1);
+    else return AkkermanFunction(M - 1, AkkermanFunction(M, n - 1));
+}
+ */
+
+/*  Задайте значения M и n. Напишите программу, которая выведет все натуральные числа в промежутке от M до n.
+M = 1; n = 5. -> ""1, 2, 3, 4, 5""
+M = 4; n = 8. -> ""4, 6, 7, 8"" */
+
+
+System.Console.WriteLine("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Все натуральные числа в промежутке от M до N: ");
+for (int i = M; i <= N; i++)
+{
+    if (i < N) System.Console.Write($"{i}, ");
+    
+    else if (i == N) System.Console.Write($"{i}");
+}
+System.Console.WriteLine();
+/* Задайте значения M и n. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до n.
+M = 1; n = 15 -> 120
+M = 4; n = 8. -> 30 */
+
+int sum = 0;
+
+for (int i = M; i <= N; i++)
+{
+    sum += i;
+}
+System.Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}");
+
+/* Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 29 */
+
+System.Console.WriteLine("Введите первое неотрицательное число n: ");
+uint numN = Convert.ToUInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите второе неотрицательное число m: ");
+uint numM = Convert.ToUInt32(Console.ReadLine());
+
+
+
+static uint A(uint n, uint m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return A(n - 1, 1);
+    else
+      return A(n - 1, A(n, m - 1));
+}
+System.Console.WriteLine($"Значения функции Аккермана: {A(numN,numM)}");
